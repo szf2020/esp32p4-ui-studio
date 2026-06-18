f24e948fbe43fc2aba4bda975933b33881b3fb51
Scott Forster <papamoascotty@gmail.com>
2026-06-18 20:57:47 +1200

    FORGEUI_WIDGET_BATCH2_COMPLETE__BUTTONMATRIX_CANVAS_LINE_TABVIEW_TILEVIEW__P4_PROVEN__SIDEBAR_AUDIT_NEXT__2026-06-18

--- firmware/ForgeUI-One/main/90_Studio_Export.c
+++ firmware/ForgeUI-One/main/90_Studio_Export.c
@@ -6,15 +6,15 @@
 
 void fg_studio_export_create(lv_obj_t *parent)
 {
-    // Background flavour: Neural Core
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x05030A), 0);
+    // Background flavour: Neon Horizon
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x0B1020), 0);
     lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_color(parent, lv_color_hex(0x05030A), 0);
+    lv_obj_set_style_bg_color(parent, lv_color_hex(0x0B1020), 0);
     lv_obj_set_style_bg_opa(parent, LV_OPA_COVER, 0);
 
-    LV_IMAGE_DECLARE(fg_upload_1024x600_neural_core_67dd4ba0);
+    LV_IMAGE_DECLARE(fg_upload_1024x600_neon_horizon_6dae04db);
     lv_obj_t * bg_texture_0 = lv_image_create(parent);
-    lv_image_set_src(bg_texture_0, &fg_upload_1024x600_neural_core_67dd4ba0);
+    lv_image_set_src(bg_texture_0, &fg_upload_1024x600_neon_horizon_6dae04db);
     lv_obj_set_pos(bg_texture_0, 0, 0);
     lv_obj_set_size(bg_texture_0, 1024, 600);
 
